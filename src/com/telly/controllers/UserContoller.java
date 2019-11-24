3dcb33a38844e871ddeb8c61ba91a7a3e1df66a7
Chacha Saber <gs_chacha@esi.dz>
2019-11-24 11:09:45 +0100

    deconnexion

--- src/com/telly/controllers/UserContoller.java
+++ src/com/telly/controllers/UserContoller.java
@@ -31,5 +31,11 @@ public class UserController {
     @RequestMapping("/login")
 	public String showLogin() {
 		return "login";
+    }
+    
+	@RequestMapping("/loggedout")
+	public String showLogout() {
+		return "loggedout";
 	}
+	
 }
